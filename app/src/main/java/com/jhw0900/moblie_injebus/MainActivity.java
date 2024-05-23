3d0ac792469bb42bb330a54e2254e6b590ff4b4b
jhw0900 <jhw0900@gmail.com>
2024-05-23 21:17:55 +0900

    로그인 화면 구현 및 로그인 API 연결

--- app/src/main/java/com/jhw0900/moblie_injebus/MainActivity.java
+++ app/src/main/java/com/jhw0900/moblie_injebus/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jhw0900.moblie_injebus;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.jhw0900.moblie_injebus.data.service.AuthenticationService;
+
 public class MainActivity extends AppCompatActivity {
+    AuthenticationService authService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        authService = new AuthenticationService();
+    }
+
+    public void onClickLogin(View v){
+        EditText eId = findViewById(R.id.id);
+        EditText ePw = findViewById(R.id.password);
+
+        String id = eId.getText().toString();
+        String pw = ePw.getText().toString();
+
+        authService.login(id, pw);
     }
 }
