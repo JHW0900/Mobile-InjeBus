923abcc50cf67af62873a15b395aaa4c89fd5f08
jhw0900 <jhw0900@gmail.com>
2024-05-23 20:30:52 +0900

    add OkHttp lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // Gson
     implementation("com.google.code.gson:gson:2.8.6")
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.9.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
