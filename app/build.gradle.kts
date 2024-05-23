0d50bb94770a74b61e1a8a521c6bccd5a2fb3bc3
jhw0900 <jhw0900@gmail.com>
2024-05-23 20:26:21 +0900

    add Gson Converter lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     // Retrofit with Scalar Converter
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    // Retrofit with Gson Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // Gson
     implementation("com.google.code.gson:gson:2.8.6")
     testImplementation(libs.junit)
